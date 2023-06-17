06f71dc19db0d2e4751030a519b773a391155d40
Liza Iutina <iutinaelizaveta@yandex.ru>
2023-06-17 12:01:30 +0300

    feat: add navbar in page main

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,7 +1,21 @@
 import "./NavTab.css";
 
+import { NavLink } from "react-router-dom";
+
 function NavTab() {
-  return <div className="NavTab"></div>;
+  return (
+    <nav className="nav">
+      <NavLink to="/#about" className="nav__link">
+        О проекте
+      </NavLink>
+      <NavLink to="#Techs" className="nav__link">
+        Технологии
+      </NavLink>
+      <NavLink to="/#AboutMe" className="nav__link">
+        Студент
+      </NavLink>
+    </nav>
+  );
 }
 
 export default NavTab;
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,22 +1,11 @@
 import "./Promo.css";
-
-import { NavLink } from "react-router-dom";
+import NavTab from "../NavTab/NavTab";
 
 function Promo() {
   return (
     <section className="promo">
       <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
-      <nav className="promo__nav">
-        <NavLink to="/#about" className="promo__link">
-          О проекте
-        </NavLink>
-        <NavLink to="#Techs" className="promo__link">
-          Технологии
-        </NavLink>
-        <NavLink to="/#AboutMe" className="promo__link">
-          Студент
-        </NavLink>
-      </nav>
+      <NavTab />
     </section>
   );
 }
