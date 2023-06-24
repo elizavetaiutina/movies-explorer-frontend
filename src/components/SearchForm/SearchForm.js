224b1ffc4acd93b6a830fc9c5bf66e795f871a74
Liza Iutina <iutinaelizaveta@yandex.ru>
2023-06-25 00:40:05 +0300

    fix: fixes

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,19 +1,38 @@
 import "./SearchForm.css";
 import searchicon from "../../images/search.svg";
-
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
+import { useEffect } from "react";
+import { useForm } from "../../hooks/useForm";
+
 function SearchForm() {
+  const { values, handleChange, setValues } = useForm({});
+
+  const resetForm = () => {
+    setValues({ name: "", email: "", password: "" });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("поиск");
+  };
+
+  useEffect(() => {
+    resetForm();
+  }, []);
+
   return (
     <div className="search">
       <div className="search__box">
-        <form className="search-form">
+        <form className="search-form" onSubmit={handleSubmit}>
           <input
             className="search-form__input"
             type="text"
             name="search"
             id="search"
             placeholder="Фильм"
+            value={values.search || ""}
+            onChange={handleChange}
             required
           />
           <button type="submit" className="search-form__button" />
